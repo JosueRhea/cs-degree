2a8f26b7c8bbfca9b1f97795d35fb7959d69fc72
JosueRhea <josueroyale8171@gmail.com>
2023-10-31 21:53:16 -0600

    Use make

--- cpp/build.sh
+++
@@ -1,9 +0,0 @@
-set -xe
-
-mkdir -p build
-
-g++ -o build/if-then if-then.cpp
-g++ -o build/if-then-2 if-then-2.cpp
-g++ -o build/switch-case switch-case.cpp
-g++ -o build/atm-example atm-example.cpp
-g++ -o build/exchange-rate-switch exchange-rate-switch.cpp
